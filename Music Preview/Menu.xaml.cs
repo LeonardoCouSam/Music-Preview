9317df017e113277aade203c29d4457e7684acd7
rudolfsmoraes <rodolfomsouza@gmail.com>
2022-04-11 20:25:09 -0300

    Arquivos Atualizados

---
+++ Music Preview/Menu.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Music_Preview
+{
+    /// <summary>
+    /// Interaction logic for Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Suporte(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Biblioteca(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_Sair(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_FinalizarCompra(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
